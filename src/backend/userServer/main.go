37222d620c1869ba7560353fd111fea1388bff10
manlio <manpuglisi94@gmail.com>
2021-11-20 17:36:24 +0100

    aggiunto middleware per scrivere le risposte in formato JSON (funzionante, testato)

--- src/backend/userServer/main.go
+++ src/backend/userServer/main.go
@@ -7,7 +7,7 @@ import (
 	"net/http"
 
 	"./controllers"
-	"./models"
+	"./middlewares"
 
 	"github.com/gorilla/mux"
 )
@@ -16,37 +16,22 @@ func homeRoot(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode("Main Endpoint root /")
 }
 
-func getUser(w http.ResponseWriter, r *http.Request) {
-	utente := &models.Utente{
-		CodFisc:   "asd",
-		Nome:      "a",
-		Cognome:   "b",
-		Email:     "c@d.e",
-		Password:  "abcde",
-		Citta:     "f",
-		Cellulare: "123",
-		Genere:    "g",
-	}
-
-	json.NewEncoder(w).Encode(utente)
-
-}
-
 func handleRequests() {
 
 	requestHandler := mux.NewRouter().StrictSlash(true)
-
 	/*Il discorso consiste che questi SubRouter gestiscono delle, appunto,
-	 *Sub-route; sono a tutti gli effetti dei router che però SONO GENERATI
+	 *Sotto-route; sono a tutti gli effetti dei router che però SONO GENERATI
 	 *dal router principale - qui definito come requestHandler. Questi sono dei
 	 *puntatori al tipo Router e quindi possono essere mandati in input agli
 	 *handler delle sotto-route.
 	**/
+
+	requestHandler.Use(middlewares.GlobalHeaders)
+
 	pazienteSubrouter := requestHandler.PathPrefix("/paziente").Subrouter()
 	psicologoSubrouter := requestHandler.PathPrefix("/psicologo").Subrouter()
 
 	requestHandler.HandleFunc("/", homeRoot)
-	requestHandler.HandleFunc("/getUser", getUser)
 
 	controllers.PazientHandler(pazienteSubrouter)
 	controllers.PsicologoHandler(psicologoSubrouter)
